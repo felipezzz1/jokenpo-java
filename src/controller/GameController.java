0bca33815d32281752717163c523be3e73de0d19
felipezzz1 <felipezorzo78@gmail.com>
2026-01-12 21:17:20 -0300

    fix - translate

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -5,13 +5,13 @@ import model.Result;
 
 public class GameController {
     public Result play(Hand player1, Hand player2) {
-        if (player1.getGesto().getGanha().contains(player2.getGesto())){
-            return Result.GANHA;
+        if (player1.getAction().getWin().contains(player2.getAction())){
+            return Result.WIN;
         }
-        if(player1.getGesto().getPerde().contains(player2.getGesto())){
-            return Result.PERDE;
+        if(player1.getAction().getLose().contains(player2.getAction())){
+            return Result.LOSE;
         }
 
-        return Result.EMPATE;
+        return Result.DRAW;
     }
 }
